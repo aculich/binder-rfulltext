77fd99240cb42d426a76958640d1c3ffe99a58b2
Aaron Culich <aculich@berkeley.edu>
2021-11-24 18:17:40 -0800

    Add install.R with installation of fulltext from github

---
+++ install.R
@@ -0,0 +1,11 @@
+library(devtools)
+install_github("ropensci/fulltext")
+
+#### Add above because @ck37 (Chris Kennedy) recommends:
+####   I’d use the github version, which is often more
+####   up-to-date anyway: https://github.com/ropensci/fulltext
+
+#### Below may become no longer installable because:
+####    Package ‘fulltext’ was removed from the CRAN repository
+####    and archived on 2021-11-22 for policy violation.
+# install.packages("fulltext")
